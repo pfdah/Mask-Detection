ec9eabc94475a30e60992f9adc9bedee7a9eb02f
pfdah <pratikdahal700@gmail.com>
2023-03-04 13:28:50 +0545

    created dataloader and model

---
+++ dataloader.py
@@ -0,0 +1,18 @@
+import torch
+import torchvision
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+
+def create_dataloader(path):
+    data_transforms = transforms.Compose([transforms.Resize((224,224)),
+                                       transforms.ToTensor(),                                
+                                       torchvision.transforms.Normalize(
+                                           mean=[0.485, 0.456, 0.406],
+                                           std=[0.229, 0.224, 0.225],
+    ),
+                                       ])
+    data = datasets.ImageFolder(path,transform=data_transforms)
+
+    
+    return DataLoader(data, shuffle = True, batch_size=4)
---
+++ divide_images.py
@@ -0,0 +1,37 @@
+import os 
+import shutil
+
+
+def test_train(src_folder, test_destination, train_destination):
+    sub_folders = ['with_mask', 'without_mask']
+    # specify the condition to separate files
+    condition = lambda filename: filename.endswith("5.jpg")
+
+    if not os.path.exists(test_destination):
+        os.makedirs(test_destination)
+    
+    for subfolder in sub_folders:
+        subfolder_path = os.path.join(test_destination, subfolder)
+        if not os.path.exists(subfolder_path):
+            os.makedirs(subfolder_path)
+
+    if not os.path.exists(train_destination):
+        os.makedirs(train_destination)
+    
+    for subfolder in sub_folders:
+        subfolder_path = os.path.join(train_destination, subfolder)
+        if not os.path.exists(subfolder_path):
+            os.makedirs(subfolder_path)
+            
+    for folder in os.listdir(src_folder):
+        for filename in os.listdir(os.path.join(src_folder, folder)):
+            src_file = os.path.join(src_folder, folder, filename)
+
+            # check if the file meets the condition
+            if condition(filename):
+                dest_file = os.path.join(test_destination, folder ,filename)
+            else:
+                dest_file = os.path.join(train_destination, folder, filename)
+
+            # move the file to the appropriate destination folder
+            shutil.copy(src_file, dest_file)
